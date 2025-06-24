11cf4150e16efa6b9efcbfe1f7f8edab18010098
Sesaka Aji Nursyah Bantani <sesaka.aji@sprout.co.id>
2025-06-24 18:57:19 +0700

    Extract template constants to dedicated file
    Move template-related constants to a separate file for better
    organization and maintainability. This separates template
    logic from other application constants.

---
+++ src/constants/templates.ts
@@ -0,0 +1,47 @@
+import { MemeTemplate } from '../types';
+
+// Sample meme templates (in a real app, these would come from an API)
+export const MEME_TEMPLATES: MemeTemplate[] = [
+    {
+        id: '1',
+        name: 'Drake Pointing',
+        imageUrl: 'https://i.imgflip.com/30b1gx.jpg',
+        width: 1200,
+        height: 1200,
+    },
+    {
+        id: '2',
+        name: 'Distracted Boyfriend',
+        imageUrl: 'https://i.imgflip.com/1ur9b0.jpg',
+        width: 1200,
+        height: 800,
+    },
+    {
+        id: '3',
+        name: 'Two Buttons',
+        imageUrl: 'https://i.imgflip.com/1g8my4.jpg',
+        width: 1200,
+        height: 908,
+    },
+    {
+        id: '4',
+        name: 'Change My Mind',
+        imageUrl: 'https://i.imgflip.com/24y43o.jpg',
+        width: 1200,
+        height: 900,
+    },
+    {
+        id: '5',
+        name: 'This Is Fine',
+        imageUrl: 'https://i.imgflip.com/26am.jpg',
+        width: 1200,
+        height: 675,
+    },
+    {
+        id: '6',
+        name: 'Woman Yelling at Cat',
+        imageUrl: 'https://i.imgflip.com/345v97.jpg',
+        width: 1200,
+        height: 438,
+    },
+]; 
